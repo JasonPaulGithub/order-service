6fcdd82a8fff74f589a1ff0528d453c8fef09e11
jason.burley <jason.burley@sedex.com>
2023-10-13 21:35:06 +0100

    Order-Service

---
+++ src/main/java/com/jasonpaul/orderservice/model/Order.java
@@ -0,0 +1,22 @@
+package com.jasonpaul.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade =  CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
+}
